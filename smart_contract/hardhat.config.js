3820f5a21587156fb591e049132ec68e5e91d88f
naimroslan <heyitsnaim@gmail.com>
2022-09-01 10:43:40 +0800

    Deleted Alchemy API key

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -4,7 +4,7 @@ module.exports = {
   solidity: '0.8.0',
   networks: {
     ropsten: {
-      url: 'https://eth-ropsten.alchemyapi.io/v2/L7AUgUITsHAHLDlwfnHQBR-e-x9tK0Kf',
+      url: 'https://eth-ropsten.alchemyapi.io/v2/{YOUR_API_KEY}',
       accounts: [ 'f46316435a0fcece15a8aa0c2b864e0896389bbd1c925e8bbf97be6b81b9cacf' ]
     }
   }
