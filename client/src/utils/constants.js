47da85b9e1ec14e0d78a2ad0ed3d51165d80fdf5
naimroslan <heyitsnaim@gmail.com>
2022-09-01 09:51:50 +0800

    Initial commit

---
+++ client/src/utils/constants.js
@@ -0,0 +1,4 @@
+import abi from './Transactions.json';
+
+export const contractABI = abi.abi; 
+export const contractAddress = '0x952d9064c5E6D5A379B8492739f02b8df856A5f2';
---
+++ smart_contract/hardhat.config.js
@@ -0,0 +1,11 @@
+require('@nomiclabs/hardhat-waffle');
+
+module.exports = {
+  solidity: '0.8.0',
+  networks: {
+    ropsten: {
+      url: 'https://eth-ropsten.alchemyapi.io/v2/L7AUgUITsHAHLDlwfnHQBR-e-x9tK0Kf',
+      accounts: [ 'f46316435a0fcece15a8aa0c2b864e0896389bbd1c925e8bbf97be6b81b9cacf' ]
+    }
+  }
+}
